09a8ebb842fb8565136b92ae1e09c6efcce6dad9
alainlemus <alainttlm@gmail.com>
2025-09-25 16:38:10 -0600

    feat:agrega brand logo

--- app/Providers/Filament/AdminShopPanelProvider.php
+++ app/Providers/Filament/AdminShopPanelProvider.php
@@ -39,6 +39,7 @@ class AdminShopPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Lime,
             ])
+            ->brandLogo(fn () => view('filament.admin.logo'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
---
+++ resources/views/filament/admin/logo.blade.php
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-600 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.35 2.7A1 1 0 007.6 17h8.8a1 1 0 00.95-.68L21 13M7 13V6h13" />
+</svg>
+<span class="ml-6 font-bold text-lg text-gray-900 dark:text-white align-middle"> Cart Recovery</span>
